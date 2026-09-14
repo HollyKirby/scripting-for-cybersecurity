4f879d6ca17a712d3607ace9ad27419bc5b6c967
HollyKirby <hollykirby3@hotmail.com>
2026-09-14 11:05:18 +0000

    Complete Lab 1

---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+student_id = input("What is your student ID? ")
+year = input("What year are you in? ")
+
+print("Name: " + name )
+print("Student Number: " + student_id)
+print("Year: " + year)
